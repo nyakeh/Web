0a466deb662eac8f5a4215b980c9c0c8185c0bfc
Nyakeh Rogers <nyakeh123@gmail.com>
2013-10-28 14:55:59 +0000

    Changed form types to use new HTML5 types

--- Assignment/Assignment/script.js
+++ Assignment/Assignment/script.js
@@ -1,41 +1,13 @@
-/*alert("hello world");*/
-/*
-$('register').on('click', function() { alert("hello ");});
-
-$('button').on('click', function() { alert(" world");});
-
-$('button').on('loginDetails', function() { alert(" world");});
-
-$('div').on('click', function() {
-    alert(" world");
-});
-*/
 var btnText = 0;
 function initialise() {
-    /*var button = document.getElementById('register');
-    var username = document.getElementById('username');
-
-    button.onclick = function() {
-        alert("jaascrit ")
-        username.toggle('hide');
-    }
-
- $('button').on('click', function() {
- $('username').toggleClass('hide');
- });
-*/
-    $('#Username').toggleClass('show');
-    /*alert("hi");*/
-    /*$('#register').on('click', function() {
-        $('#Username').toggleClass('show');
-    });*/
+     $('#Username').toggleClass('show');
 }
 function changeText() {
     $('#Username').toggleClass('show');
     if (btnText == 0){
         btnText = 1;
         $('#register').html('login');
-        $('#new').html('<p><label>Email Address: <input type="text" name="email"></label></p>');
+        $('#new').html('<p><label>Email Address: <input type="email" name="email" id="email"> <span id="emailMsg"></span></label></p>');
     } else {
         $('#register').html('register');
         $('#new').html('');
