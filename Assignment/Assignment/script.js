13df90f2fa9ecf761e2c7cebcc1c4068f6186728
Nyakeh Rogers <nyakeh123@gmail.com>
2013-10-28 16:51:48 +0000

    Client side form validation

--- Assignment/Assignment/script.js
+++ Assignment/Assignment/script.js
@@ -1,33 +1,35 @@
 var btnText = 0;
-window.onload=initialise;
-function initialise() {
+function initialise()
+{
+    registerValidation(document.getElementById('url'), document.getElementById('urlMsg'));
+    registerValidation(document.getElementById('email'), document.getElementById('emailMsg'));
+    registerValidation(document.getElementById('userPassword'), document.getElementById('passwordMsg'));
+    registerValidation(document.getElementById('username'), document.getElementById('usernameMsg'));
+    /**/
     registerValidation(
-        document.getElementById('emailAddress'),
-        document.getElementById('emailMsg')
-    );
-    registerValidation(
-        document.getElementById('userPassword'),
-        document.getElementById('passwordMsg')
+        document.getElementById('number'),
+        document.getElementById('numMsg')
     );
 }
-
 function registerValidation(element, span)
 {
-    element.onblur = function() {
+    element.onblur = function()  {
         span.textContent = element.validationMessage;
-        alert(element.validationMessage);
     }
 }
-
 function changeText() {
     $('#Username').toggleClass('show');
     if (btnText == 0){
         btnText = 1;
         $('#register').html('login');
-        $('#new').html('<p><label>Email Address: <input type="email" name="emailAddress" id="emailAddress"> <span id="emailMsg"></span></label></p>');
+        $('#new').html('<p><label>Email Address: <input type="email" name="mail" id="mail"> <span id="mailMsg"></span></label></p>');
+
+        registerValidation(document.getElementById('mail'), document.getElementById('mailMsg'));
     } else {
         $('#register').html('register');
         $('#new').html('');
         btnText = 0;
     }
 }
+
+window.onload=initialise;
