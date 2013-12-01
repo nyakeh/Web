4ad334a0a0e9ab80f00cd7057c98b42a91b1df51
Nyakeh Rogers <nyakeh123@gmail.com>
2013-12-01 17:36:22 +0000

    Design: created cloud art
    Started creating footer

--- Parallax/parallax.js
+++ Parallax/parallax.js
@@ -58,7 +58,6 @@ $(document).ready(function() {
 function parallaxScroll(){
     var scrolled = $(window).scrollTop();
     $('#parallax-bg1').css('top',(0-(scrolled*.25))+'px');
-    $('#parallax-bg2').css('top',(0-(scrolled*.5))+'px');
     $('#parallax-bg3').css('top',(0-(scrolled*.75))+'px');
 }
 
