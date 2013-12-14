cade0740de327c0d883148a5a6f56c9867043491
Nyakeh Rogers <nyakeh123@gmail.com>
2013-12-14 15:31:06 +0000

    Created cool Sticky Header site/layout
    Header that gets smaller as you scroll down a page.
    Followed
    'https://www.skillfeed.com/courses/1053-building-a-sticky-header?video_id=18576-using-jquery'.
    Separated out JS to separate file.

---
+++ Sticky/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if($(window).scrollTop() >= 100) {
+            $('header').css('padding','0');
+            $('h1 img').attr('src', "img/logoalt.png")
+        } else {
+            $('header').css('padding','25px 0');
+            $('h1 img').attr('src', "img/logo.png")
+        }
+    });
+});
