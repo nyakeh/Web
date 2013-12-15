c97e2790f2f2b0981854a3a4dd30153573291630
Nyakeh Rogers <nyakeh123@gmail.com>
2013-12-15 02:55:02 +0000

    Building new site
    Merging previous styles.
    Created header with background pattern.
    Styled nav list.
    TODO - sub-lists in nav

---
+++ Sticky/freshScript.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if($(window).scrollTop() >= 120) {
+            $('header').css('padding','0');
+            $('h1 img').attr('src', "img/logoalt.png")
+        } else {
+            $('header').css('padding','25px 0');
+            $('h1 img').attr('src', "img/logo.png")
+        }
+    });
+});
