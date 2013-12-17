a0e7bdc9a1146b492383fdcae2ace77d0b121ef6
Nyakeh Rogers <nyakeh123@gmail.com>
2013-12-17 13:14:49 +0000

    Finished header & Welcome banner
    Styled content & footer.

--- Sticky/freshScript.js
+++ Sticky/freshScript.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
     $(window).scroll(function() {
         if($(window).scrollTop() >= 120) {
             $('header').css('padding','0');
+            $('header').css('background','#000');
             $('h1 img').attr('src', "img/logoalt.png")
         } else {
             $('header').css('padding','25px 0');
+            $('header').css('background','rgba(0,0,0, 0.3)');
             $('h1 img').attr('src', "img/logo.png")
         }
     });
