eeef9090d80dffdd4d72b677ca5d7ee8178fa3e8
Nyakeh Rogers <nyakeh123@gmail.com>
2013-12-18 23:05:18 +0000

    Responsive Design
    Made site more optimised for smaller screen sizes.
    Made NAV menu hide when screen gets a certain size.

--- Sticky/freshScript.js
+++ Sticky/freshScript.js
@@ -1,13 +1,37 @@
 $(document).ready(function() {
+    myFunction();
     $(window).scroll(function() {
         if($(window).scrollTop() >= 120) {
             $('header').css('padding','0');
-            $('header').css('background','#000');
-            $('h1 img').attr('src', "img/logoalt.png")
+            $('header').css('background','rgba(0,0,0, 0.8)');
+            /*$('h1 img').attr('src', "img/logoalt.png")*/
         } else {
             $('header').css('padding','25px 0');
             $('header').css('background','rgba(0,0,0, 0.3)');
-            $('h1 img').attr('src', "img/logo.png")
+            /*$('h1 img').attr('src', "img/logo.png")*/
         }
     });
+
+
 });
+$(window).addEventListener('resize', myFunction()) ;
+navBool = true;
+function showNav() {
+    if(navBool) {
+        $('#nav>li').css('display','list-item');
+        navBool = false;
+    } else {
+            $('#nav>li').css('display','none');
+            navBool = true;
+    }
+};
+
+function myFunction() {
+    if($(window).width() >= 659) {
+        $('#nav>li').css('display','block');
+        navBool = false;
+    } else {
+        $('#nav>li').css('display','none');
+        navBool = true;
+    }
+};
