e32b5eb3cbd5f61c9d3c9cb81cb531426d621acd
Nyakeh Rogers <nyakeh123@gmail.com>
2014-01-10 10:31:10 +0000

    Switched from mobile first to desktop first
    Switched media queries so site starts large, and responds to smaller
    screens rather then vice versa.
    Started adding btn to hide nav on mobile devices.

--- Sticky/ResponsiveScript.js
+++ Sticky/ResponsiveScript.js
@@ -1,4 +1,14 @@
-$('.nav-primary').find('h3')
-    .bind('click focus', function(){
-        $(this).parent().toggleClass('expanded')
+
+$(document).ready(function() {
+    
+    $('a#menu_btn').click(function() {
+        alert("hi");
+        $('H1').toggleClass('open_menu');
     });
+
+    document.getElementById('menu_btn').click(function() {
+        alert("hello");
+    });
+
+
+});
