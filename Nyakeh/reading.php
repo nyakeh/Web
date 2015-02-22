ecaf3ed80fae8c0778243d23dc0642d74fe9848f
Nyakeh Rogers <nyakeh123@gmail.com>
2015-02-22 22:25:19 +0000

    Finished initial template for book 'cards'

--- Nyakeh/reading.php
+++ Nyakeh/reading.php
@@ -38,46 +38,79 @@
 	<div class="section container hero">
 		<div class="row">
 			<div class="one-half column">
-				  <i class="fa fa-book fa-3x bookIcon" ></i>
+				<i class="fa fa-book fa-3x bookIcon"></i>
 			</div>
 			<div class="one-half column">
 				<h1 class="hero-heading">Reading List</h1>
 			</div>
 		</div>
 	</div>
-	
+
 	<div class="container">
 		<div class="row">
+			<h3>Currently Reading</h3>
+			<div class="book">
+				<div class="one-half column">
+					<img class="u-max-full-width" src="img/softSkills.jpg">
+				</div>
+				<div class="one-half column bookDetails">
+					<h4 class="bookTitle">Soft Skills</h4>
+					<h5 class="bookTitle">By John Sonmez</h5>
+					<p>A unique guide, offering techniques and practices for a more satisfying life as a professional software developer. In it, developer and life coach John Sonmez addresses a wide range of important "soft" topics, from career and productivity to personal finance and investing, and even fitness and relationships, all from a developer-centric viewpoint.</p>
+				</div>
+				<div class="progress"></div>
+			</div>
+			<div class="book">
+				<div class="one-half column">
+					<img class="u-max-full-width" src="img/softSkills.jpg">
+				</div>
+				<div class="one-half column bookDetails">
+					<h4 class="bookTitle">Soft Skills</h4>
+					<h5 class="bookTitle">By John Sonmez</h5>
+					<p>A unique guide, offering techniques and practices for a more satisfying life as a professional software developer. In it, developer and life coach John Sonmez addresses a wide range of important "soft" topics, from career and productivity to personal finance and investing, and even fitness and relationships, all from a developer-centric viewpoint.</p>
+				</div>
+				<div class="progress"></div>
+			</div>
+			<div class="book">
+				<div class="one-half column">
+					<img class="u-max-full-width" src="img/softSkills.jpg">
+				</div>
+				<div class="one-half column bookDetails">
+					<h4 class="bookTitle">Soft Skills</h4>
+					<h5 class="bookTitle">By John Sonmez</h5>
+					<p>A unique guide, offering techniques and practices for a more satisfying life as a professional software developer. In it, developer and life coach John Sonmez addresses a wide range of important "soft" topics, from career and productivity to personal finance and investing, and even fitness and relationships, all from a developer-centric viewpoint.</p>
+				</div>
+				<div class="progress"></div>
+			</div>
 		</div>
-	</div>
 
-	<div class="footer">
-		<ul>
-			<li>
-				<a href="https://github.com/nyakeh123">
-					<img src="img/github.png">
-				</a>
-			</li>
-			<li>
-				<a href="http://instagram.com/nyakehrogers">
-					<img class="icon-padding" src="img/instagram.png">
-				</a>
-			</li>
-			<li>
-				<a href="http://lnkd.in/bGufZiJ">
-					<img class="icon-padding" src="img/linkedin.png">
-				</a>
-			</li>
-			<li>
-				<a href="https://twitter.com/nyakehrogers">
-					<img class="icon-padding" src="img/twitter.png">
-				</a>
-			</li>
-		</ul>
-	</div>
+		<div class="footer">
+			<ul>
+				<li>
+					<a href="https://github.com/nyakeh123">
+						<img src="img/github.png">
+					</a>
+				</li>
+				<li>
+					<a href="http://instagram.com/nyakehrogers">
+						<img class="icon-padding" src="img/instagram.png">
+					</a>
+				</li>
+				<li>
+					<a href="http://lnkd.in/bGufZiJ">
+						<img class="icon-padding" src="img/linkedin.png">
+					</a>
+				</li>
+				<li>
+					<a href="https://twitter.com/nyakehrogers">
+						<img class="icon-padding" src="img/twitter.png">
+					</a>
+				</li>
+			</ul>
+		</div>
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="/js/script.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script src="/js/script.js"></script>
 </body>
 
 </html>
