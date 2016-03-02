0df50808d1087248f3e15e693e56e7a43c73e943
Nyakeh Rogers <nyakeh123@gmail.com>
2016-03-02 19:02:11 +0000

    Extracted chart javascript to separate class

--- Nyakeh/js/script.js
+++ Nyakeh/js/script.js
@@ -1,8 +1,45 @@
-$("#deskImage").click(function(){
+$("#deskImage").click(function () {
     imagePath = $("#deskImage").attr("src");
-    if(imagePath == "/img/desk.png"){
+    if (imagePath == "/img/desk.png") {
         $("#deskImage").attr("src", "/img/deskAlt.png");
-    }else{
-        $("#deskImage").attr("src", "/img/desk.png");        
+    } else {
+        $("#deskImage").attr("src", "/img/desk.png");
     }
+});
+
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
+
+var ctx = $("#financeChart").get(0).getContext("2d");
+var myLineChart = new Chart(ctx);
+myLineChart.Line(data, {
+    pointDotRadius: 10,
+    bezierCurve: true,
+    scaleShowVerticalLines: false,
+    scaleGridLineColor: "black",
+    responsive: true,
+    maintainAspectRatio: true
 });
