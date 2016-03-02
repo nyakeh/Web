ce563f78aa85a28b2a015c8974f6d8ec340f56cd
Nyakeh Rogers <nyakeh123@gmail.com>
2016-03-02 19:22:21 +0000

    Styled the line graph - ready for some real data!

--- Nyakeh/js/script.js
+++ Nyakeh/js/script.js
@@ -8,27 +8,31 @@ $("#deskImage").click(function () {
 });
 
 var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
     datasets: [
         {
             label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
+            fillColor: "rgba(71,121,101,0.2)",
+            strokeColor: "rgba(71,121,101,1)",
+            pointColor: "rgba(71,121,101,1)",
+            pointHighlightFill: "#000",
             data: [65, 59, 80, 81, 56, 55, 40]
         },
         {
             label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
+            fillColor: "rgba(176,0,17,0.2)",
+            strokeColor: "rgba(176,0,17,1)",
+            pointColor: "rgba(176,0,17,1)",
+            pointHighlightFill: "#000",
             data: [28, 48, 40, 19, 86, 27, 90]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(121,101,71,0.2)",
+            strokeColor: "rgba(121,101,71,1)",
+            pointColor: "rgba(121,101,71,1)",
+            pointHighlightFill: "#000",
+            data: [12, 11, 1, 21, 14, 12, 16]
         }
     ]
 };
@@ -36,7 +40,7 @@ var data = {
 var ctx = $("#financeChart").get(0).getContext("2d");
 var myLineChart = new Chart(ctx);
 myLineChart.Line(data, {
-    pointDotRadius: 10,
+    pointDotRadius: 6,
     bezierCurve: true,
     scaleShowVerticalLines: false,
     scaleGridLineColor: "black",
