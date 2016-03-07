b175401b1af96bb13b6d1f1a10897f23ba27c882
Nyakeh Rogers <nyakeh123@gmail.com>
2016-03-07 19:32:09 +0000

    Use a fake dictionary to map the month into its index value for the sql statement

--- Nyakeh/dash/Expenses_Function.php
+++ Nyakeh/dash/Expenses_Function.php
@@ -2,6 +2,20 @@
     include('database.php');
     global $conn;
     
+    $monthIndex = array(
+    "january" => 1,
+    "february" => 2,
+    "march" => 3,
+    "april" => 4,
+    "may" => 5,
+    "june" => 6,
+    "july" => 7,
+    "august" => 8,
+    "september" => 9,
+    "october" => 10,
+    "november" => 11,
+    "december" => 12);
+    
     class ExpenseDetails {
         public $Category = "";
         public $Amount = 0;
@@ -11,11 +25,12 @@
     $month = $_POST['month'];
     $year = $_POST['year'];
     
-    $result = mysqli_query($conn,"SELECT * FROM `current_account` WHERE MONTH(date) = 2 and YEAR(date) = " . $year);
+    $result = mysqli_query($conn,"SELECT * FROM `current_account` WHERE MONTH(date) = ".$monthIndex[$month]." and YEAR(date) = " . $year);
     
     while($row = mysqli_fetch_assoc($result)){
         /*echo "ID: ".$row['id'].", Value:".$row['Value'] .", Category:".$row['Category'].", Date:".$row['Date']."<br/>";*/
     }
+    
     $piecesOfPie = new ExpenseDetails();
     $piecesOfPie->Category = "nyakeh";
     $piecesOfPie->Amount = 200;
