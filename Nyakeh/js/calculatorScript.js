437203ff77099f02eb4a79d23550d40168e0e824
Nyakeh Rogers <nyakeh123@gmail.com>
2016-03-14 19:40:13 +0000

    Implemented Compound interest calculator for monthly deposits. Formatted cash amounts to 2 decimal places. Added output table to display results.

--- Nyakeh/js/calculatorScript.js
+++ Nyakeh/js/calculatorScript.js
@@ -4,12 +4,19 @@ const MONTHS_PER_YEAR = 12;
 
 $('#calculatorSubmit').click(function() {
     var costOfLiving = $('#costOfLivingInput').val();
-    var monthlySavings = $('#monthlySavingsInput').val();
-    var portfolioValue = $('#portfolioValueInput').val();
+    var monthlySaving = $('#monthlySavingsInput').val();
+    var portfolio = $('#portfolioInput').val();
 
-    var monthlyInterestRate = (1+ANNUAL_RETURN_ON_INVESTMENT/MONTHS_PER_YEAR);
-    var monthsInTerm = MONTHS_PER_YEAR*(10);
-    var termInterestRate = Math.pow(monthlyInterestRate,monthsInTerm)
-    var endPrincipalAmount = portfolioValue * termInterestRate;
-    console.log(endPrincipalAmount);
+    var monthlyInterestRate = (ANNUAL_RETURN_ON_INVESTMENT / MONTHS_PER_YEAR);
+    var monthsInTerm = MONTHS_PER_YEAR * (10);
+    
+    var termInterestRate = Math.pow((1 + monthlyInterestRate), monthsInTerm);
+    var principalReturn = portfolio * termInterestRate;
+    
+    var compoundTermInterestRate = (termInterestRate-1) / monthlyInterestRate;
+    var savingsReturn = monthlySaving * compoundTermInterestRate;
+
+    $('#principalReturn').text('£' + principalReturn.toFixed(2));
+    $('#savingsReturn').text('£' + savingsReturn.toFixed(2));
+    $('#futureNetWorth').text('£' + (principalReturn + savingsReturn).toFixed(2));
 })
