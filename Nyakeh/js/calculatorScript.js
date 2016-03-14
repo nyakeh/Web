0d1f07ecbe356fee4864614d1e0e7bf1aead4d64
Nyakeh Rogers <nyakeh123@gmail.com>
2016-03-14 20:10:48 +0000

    Workout years till retirement is viable with input saving rate.

--- Nyakeh/js/calculatorScript.js
+++ Nyakeh/js/calculatorScript.js
@@ -6,28 +6,36 @@ $('#calculatorSubmit').click(function() {
     var costOfLiving = $('#costOfLivingInput').val();
     var monthlySaving = $('#monthlySavingsInput').val();
     var portfolio = $('#portfolioInput').val();
-    var years = $('#yearsInput').val();
 
+    var requiredInvestedAmount = costOfLiving / WITHDRAWAL_RATE;
+    var netWorth = portfolio;
+    var years = 0;
     var monthlyInterestRate = (ANNUAL_RETURN_ON_INVESTMENT / MONTHS_PER_YEAR);
-    var monthsInTerm = MONTHS_PER_YEAR * years;
     
-    var termInterestRate = Math.pow((1 + monthlyInterestRate), monthsInTerm);
-    var principalReturn = portfolio * termInterestRate;
-    
-    var compoundTermInterestRate = (termInterestRate-1) / monthlyInterestRate;
-    var savingsReturn = monthlySaving * compoundTermInterestRate;
+    while (netWorth < requiredInvestedAmount) {
+        years++;
+        var monthsInTerm = MONTHS_PER_YEAR * years;
+
+        var termInterestRate = Math.pow((1 + monthlyInterestRate), monthsInTerm);
+        var principalReturn = portfolio * termInterestRate;
+
+        var compoundTermInterestRate = (termInterestRate - 1) / monthlyInterestRate;
+        var savingsReturn = monthlySaving * compoundTermInterestRate;
+        netWorth = principalReturn + savingsReturn;
+    }
 
     $('#principalReturn').text('£' + principalReturn.formatMoney());
     $('#savingsReturn').text('£' + savingsReturn.formatMoney());
-    $('#futureNetWorth').text('£' + (principalReturn + savingsReturn).formatMoney());
+    $('#futureNetWorth').text('£' + netWorth.formatMoney());
+    $('#yearsTillRetirement').text(years + 'years');
 })
 
-Number.prototype.formatMoney = function(){
-var n = this,
-    c = 2,
-    t = ",",
-    s = n < 0 ? "-" : "", 
-    i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", 
-    j = (j = i.length) > 3 ? j % 3 : 0;
-   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? "." + Math.abs(n - i).toFixed(c).slice(2) : "");
- };
+Number.prototype.formatMoney = function() {
+    var n = this,
+        c = 2,
+        t = ",",
+        s = n < 0 ? "-" : "",
+        i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
+        j = (j = i.length) > 3 ? j % 3 : 0;
+    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? "." + Math.abs(n - i).toFixed(c).slice(2) : "");
+};
