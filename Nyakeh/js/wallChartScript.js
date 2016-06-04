872674a2efe417260abfd3e09c315959179ee19e
Nyakeh Rogers <nyakeh123@gmail.com>
2016-06-04 16:16:07 +0100

    Display all persisted monthly check-ins in a line chart. Added some whitespace between the line chart & table.

--- Nyakeh/js/wallChartScript.js
+++ Nyakeh/js/wallChartScript.js
@@ -26,11 +26,102 @@ var calculateRetirement = function () {
 			try {
                 var results = JSON.parse(output);
                 var table = '<table><thead><tr><th>Date</th><th>Made</th><th>Spent</th><th>Invested</th></tr></thead><tbody>';
+                var chartLabels = [];
+                var madeData = [];
+                var spentData = [];
+                var investedData = [];
                 for (var i in results) {
                     table += '<tr><td>' + results[i].Date + '</td><td>£' + results[i].Made + '</td><td>£' + results[i].Spent + '</td><td>£' + results[i].Invested + '</td></tr>';
+                    chartLabels.push(results[i].Date)
+                    madeData.push(results[i].Made)
+                    spentData.push(results[i].Spent)
+                    investedData.push(results[i].Invested)
                 }
                 table += '</tbody></table>';
                 $("#checkInTable").html(table);
+                
+                var data = {
+                    labels: chartLabels,
+                    datasets: [
+                        {
+                            label: "Made",
+                            fill: false,
+                            lineTension: 0.1,
+                            backgroundColor: "rgba(75,192,192,0.4)",
+                            borderColor: "rgba(75,192,192,1)",
+                            borderCapStyle: 'butt',
+                            borderDash: [],
+                            borderDashOffset: 0.0,
+                            borderJoinStyle: 'miter',
+                            pointBorderColor: "rgba(75,192,192,1)",
+                            pointBackgroundColor: "#fff",
+                            pointBorderWidth: 1,
+                            pointHoverRadius: 5,
+                            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                            pointHoverBorderColor: "rgba(220,220,220,1)",
+                            pointHoverBorderWidth: 2,
+                            pointRadius: 1,
+                            pointHitRadius: 10,
+                            data: madeData,
+                        },
+                        {
+                            label: "Spent",
+                            fill: false,
+                            lineTension: 0.1,
+                            backgroundColor: "rgba(75,192,192,0.4)",
+                            borderColor: "rgba(75,192,192,1)",
+                            borderCapStyle: 'butt',
+                            borderDash: [],
+                            borderDashOffset: 0.0,
+                            borderJoinStyle: 'miter',
+                            pointBorderColor: "rgba(75,192,192,1)",
+                            pointBackgroundColor: "#fff",
+                            pointBorderWidth: 1,
+                            pointHoverRadius: 5,
+                            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                            pointHoverBorderColor: "rgba(220,220,220,1)",
+                            pointHoverBorderWidth: 2,
+                            pointRadius: 1,
+                            pointHitRadius: 10,
+                            data: spentData,
+                        },
+                        {
+                            label: "Invested",
+                            fill: false,
+                            lineTension: 0.1,
+                            backgroundColor: "rgba(75,192,192,0.4)",
+                            borderColor: "rgba(75,192,192,1)",
+                            borderCapStyle: 'butt',
+                            borderDash: [],
+                            borderDashOffset: 0.0,
+                            borderJoinStyle: 'miter',
+                            pointBorderColor: "rgba(75,192,192,1)",
+                            pointBackgroundColor: "#fff",
+                            pointBorderWidth: 1,
+                            pointHoverRadius: 5,
+                            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                            pointHoverBorderColor: "rgba(220,220,220,1)",
+                            pointHoverBorderWidth: 2,
+                            pointRadius: 1,
+                            pointHitRadius: 10,
+                            data: investedData,
+                        }
+                    ]
+                };
+                var wallChartCanvas = $("#wallChart");
+                var WallChart = new Chart(wallChartCanvas, {
+                    type: 'line',
+                    data: data,
+                    options: {
+                        responsive: true,
+                        elements: {
+                            line: {
+                                lineTension: 0.1
+                            }
+                        }
+                    }
+                });
+                
             } catch (exception) {
                 console.log(exception);
             }
