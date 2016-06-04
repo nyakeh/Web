d1b3d9f2b2437e702e53a799d908c17ff4f0f6bf
Nyakeh Rogers <nyakeh123@gmail.com>
2016-06-04 17:18:44 +0100

    Deleted Made vs Spent vs Savings chart, it was now duplicated in the Wall Chart page.

--- Nyakeh/js/script.js
+++ Nyakeh/js/script.js
@@ -10,49 +10,7 @@ $("#deskImage").click(function () {
 var changeExpensesMonth = function($selectedDate) {
     var dateInputs = $selectedDate.split(',');
     populateExpensesChart(dateInputs[0], dateInputs[1]);
-} 
-
-var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-    datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(71,121,101,0.2)",
-            strokeColor: "rgba(71,121,101,1)",
-            pointColor: "rgba(71,121,101,1)",
-            pointHighlightFill: "rgba(71,121,101,0.2)",
-            data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(176,0,17,0.2)",
-            strokeColor: "rgba(176,0,17,1)",
-            pointColor: "rgba(176,0,17,1)",
-            pointHighlightFill: "rgba(176,0,17,0.5)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(121,101,71,0.2)",
-            strokeColor: "rgba(121,101,71,1)",
-            pointColor: "rgba(121,101,71,1)",
-            pointHighlightFill: "rgba(121,101,71,0.5)",
-            data: [12, 11, 1, 21, 14, 12, 16]
-        }
-    ]
-};
-
-var ctx = $("#financeChart").get(0).getContext("2d");
-var myLineChart = new Chart(ctx);
-myLineChart.Line(data, {
-    pointDotRadius: 6,
-    bezierCurve: true,
-    scaleShowVerticalLines: false,
-    scaleGridLineColor: "black",
-    responsive: true,
-    maintainAspectRatio: true,
-    multiTooltipTemplate: "<%= datasetLabel %>: <%= value %>"
-});
+}
 
 var populateExpensesChart = function($month, $year) {
     $('#expensesChart').replaceWith('<canvas id="expensesChart" width="500" height="500"></canvas>');
@@ -64,7 +22,7 @@ var populateExpensesChart = function($month, $year) {
         type: 'post',
         success: function (output) {
             try {
-                var results = JSON.parse(output);                        
+                var results = JSON.parse(output);
                 var table = '<table><thead><tr><th>Category</th><th>Amount</th></tr></thead><tbody>';
                 
                 results.sort(function(a, b){
