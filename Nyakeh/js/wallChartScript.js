b7f74795cb16c62b72da3619b6a74f6f58e9700e
Nyakeh Rogers <nyakeh123@gmail.com>
2016-06-04 16:42:38 +0100

    Deleting logging used for debugging.

--- Nyakeh/js/wallChartScript.js
+++ Nyakeh/js/wallChartScript.js
@@ -4,15 +4,10 @@ $('#wallChartCheckin').submit(function () {
     var invested = $('#investedInput').val();
     var date = $('#dateInput').val();
 
-    console.log('made: ' + made + 'spent: ' + spent + 'invested: ' + invested + 'date: ' + date);
-
     $.ajax({
         url: 'Wall_Chart.php',
         data: { date: date, made: made, spent: spent, invested: invested },
-        type: 'post',
-        success: function (output) {
-            console.log('Success feedback:' + output);
-        }
+        type: 'post'
     });
 });
 
@@ -21,8 +16,6 @@ var calculateRetirement = function () {
         url: 'Wall_Chart.php',
         type: 'get',
         success: function (output) {
-            console.log('Success feedback:' + output);
-
 			try {
                 var results = JSON.parse(output);
                 var table = '<table><thead><tr><th>Date</th><th>Made</th><th>Spent</th><th>Invested</th></tr></thead><tbody>';
